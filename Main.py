ddc119344efbb144d13e0e5f0cbd4b48a26f6a2a
lstanisic <lucija.stanisic@students.unibe.ch>
2018-07-17 20:00:50 +0200

    data sets loaded

--- Main.py
+++ Main.py
@@ -1,54 +1,44 @@
-# import numpy
+import numpy
 import glob, os
 
-# class Car:
-#     def __init__(self):
-#         self.speed = 0
-#         self.odometer = 0
-#         self.time = 0
-#
-#     def say_state(self):
-#         print("I'm going {} kph!".format(self.speed))
-#
-#     def accelerate(self):
-#         self.speed += 5
-#
-#     def brake(self):
-#         self.speed -= 5
-#
-#     def step(self):
-#         self.odometer += self.speed
-#         self.time += 1
-#
-#     def average_speed(self):
-#         return self.odometer / self.time
+# settings
+load_max_files = 3
+max_rows = 100
+
+class GrainsizeSet(object):
+    name = ""
+    data = []
+
+    # def __init__(self, name, data):
+    #     self.name = name
+    #     self.data = data
 
 
-# if __name__ == '__main__':
 
-    # Look for dat files
+# declare global variables
+grainsize_sets = []
+selected_files = []
+index = 0
+
+# Look for dat files
 for root, dirs, files in os.walk("U:/Other_Stuff/Seminar_DMA/dem_reach/"):
+
+    # Get data for each file
     for file in files:
-        if file.endswith(".dat"):
-            print(os.path.join(root, file))
+        if file.endswith(".dat") and (index < load_max_files):
 
+            index += 1
 
+            filename = os.path.join(root, file)
+            print(filename)
 
-    # my_car = Car()
-    # print("I'm a car!")
-    # while True:
-    #     action = input("What should I do? [A]ccelerate, [B]rake, "
-    #         "show [O]dometer, or show average [S]peed?").upper()
-    #     if action not in "ABOS" or len(action) != 1:
-    #         print("I don't know how to do that")
-    #         continue
-    #     if action == 'A':
-    #         my_car.accelerate()
-    #     elif action == 'B':
-    #         my_car.brake()
-    #     elif action == 'O':
-    #         print("The car has driven {} kilometers".format(my_car.odometer))
-    #     elif action == 'S':
-    #         print("The car's average speed was {} kph".format(my_car.average_speed()))
-    #     my_car.step()
-    #     my_car.say_state()
+            data = numpy.genfromtxt(filename, delimiter=' ', max_rows=max_rows)
+            grainsize_set = GrainsizeSet()
+            grainsize_set.name = os.path.basename(file)
+            grainsize_set.data = data
+
+            print("Loaded" + filename)
+
+            grainsize_sets.append(grainsize_set)
+
+    print("finished")
